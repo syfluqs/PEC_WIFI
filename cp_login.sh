983e008dd1abb716251b24046368256d38aef8f0
syfluqs <syfluqs@gmail.com>
2016-06-23 14:21:02 +0530

    only login if disconnected

--- cp_login.sh
+++ cp_login.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/zsh
 
-echo $$ > pidfile
+sudo kill $(cat /home/roy/projects/PEC_WIFI/pidfile)
+echo $$ > /home/roy/projects/PEC_WIFI/pidfile
 while true;
 do
-msg=$(curl -s -d "username=$(cat /home/roy/projects/PEC_WIFI/my_cp_user)&password=$(cat /home/roy/projects/PEC_WIFI/my_cp_pass)&mode=191" http://172.31.1.220:8090/httpclient.html | sed 's/CDATA\[/\n/g' | sed 's/\]\]/\n/g' | sed -n '4p')
-echo $msg
-notify-send $msg
-sleep 120
+	if [ ! -z $(curl -s http://www.google.com | grep 172.31.1.220) ];
+		then
+			msg=$(curl -s -d "username=$(cat /home/roy/projects/PEC_WIFI/my_cp_user)&password=$(cat /home/roy/projects/PEC_WIFI/my_cp_pass)&mode=191" http://172.31.1.220:8090/httpclient.html | sed 's/CDATA\[/\n/g' | sed 's/\]\]/\n/g' | sed -n '4p')
+			echo $msg
+			notify-send "PEC Network" $msg
+	fi
+	sleep 3
 done
